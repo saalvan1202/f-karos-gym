bd9fff6b7596ff7f606b52702bedcfaf02abfe02
saalvan1202 <ashande75@gmail.com>
2025-02-16 14:46:55 -0500

    menu en inicio

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -1,22 +1,25 @@
 import { Card } from "antd";
+import MenuPrincipal from "../../components/MenuPrincipal";
 
 export default function Inicio() {
   return (
-    <div
-      style={{
-        width: "100%",
-        padding: "20px",
-      }}
-    >
-      <Card
+    <MenuPrincipal>
+      <div
         style={{
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)",
-          display: "grid",
-          placeItems: "center",
+          width: "100%",
+          padding: "20px",
         }}
       >
-        <h2>Bienvenido al sistema de ventas</h2>
-      </Card>
-    </div>
+        <Card
+          style={{
+            boxShadow: "0 4px 8px rgba(0, 0, 0, 0.15)",
+            display: "grid",
+            placeItems: "center",
+          }}
+        >
+          <h2>Bienvenido al sistema de ventas</h2>
+        </Card>
+      </div>
+    </MenuPrincipal>
   );
 }
